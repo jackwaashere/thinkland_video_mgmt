ff9a2c7a31ff438c98cb17c8ccffd76be3bbfc6d
jackwaashere <88603122+jackwaashere@users.noreply.github.com>
2023-01-08 20:05:38 -0800

    Add files via upload

--- client.py
+++ client.py
@@ -27,13 +27,13 @@ import googleapiclient.errors
 import auth
 from auth import lib
 
-from thinkland import meetingdb
+from thinkland import classes
+from thinkland.classes import log
 import make_playlists
 
 
-unprocessed_id = 'PLLoERmYbGOUn9r9pAke-3_pj-kEEj9cpl'
-meetingdb_json = 'data/meetingdb.json'
-playlist_json = 'data/playlist.json'
+unprocessed_playlist = 'PLLoERmYbGOUn9r9pAke-3_pj-kEEj9cpl'
+error_processing_playlist = 'PLLoERmYbGOUkL5PX69LpM4bUiMYjhzOk2'
 
 DAILY_THRESHOLD = 9000
 
@@ -58,6 +58,7 @@ def get_some_unprocessed_videos(youtube):
     """Try to retrieve some unprocessed videos.
     
     Returns a list of (video_id, title, playlist_item_id) tuples.
+    EDIT: {'id': video_id, 'title': video_title, 'desc': video_description, 'itemId': playlist_item_id} dict
     
     Because of the Youtube API quota. We need to limit to process up to N videos
     a day. N is set by flag (default = 10)
@@ -65,12 +66,19 @@ def get_some_unprocessed_videos(youtube):
     request = youtube.playlistItems().list(
         part='snippet,contentDetails',
         maxResults=25, # TODO: Change maxResults to flag
-        playlistId=unprocessed_id
+        playlistId=unprocessed_playlist
     )
     pl_items_list = request.execute()
     ret = list()
     for item in pl_items_list['items']:
-        ret.append((item['contentDetails']['videoId'], item['snippet']['title'], item['id']))
+        ret.append(
+            {
+                'id': item['contentDetails']['videoId'],
+                'title': item['snippet']['title'],
+                'desc': item['snippet']['description'],
+                'itemId': item['id']
+            }
+        )
     return ret
 
 def process_video(video, meeting, youtube):
@@ -84,14 +92,14 @@ def process_video(video, meeting, youtube):
     Remove this video from the unprocessed playlist by the given playlist_item_id.
     Set the video_id back to the meetingsDB.
     """
-    can_zid = video[1].split(' ')[0]  # better to get it from meeting object
-    recording_start = meeting.meetingDate + meeting.startTime
+    can_zid = video['title'].split(' ')[0]  # better to get it from meeting object
+    recording_start = meeting.startTime
     new_title = meeting.className + ' ' + meeting.teacherName + ' ' + str(recording_start)
-    new_desc = '###' + can_zid + '|' + meeting.classId + '|' + str(recording_start) + '|' + meeting.teacherName + '|' + meeting.className + '###\n###YJv1###\n' + video[1]
+    new_desc = '###' + can_zid + '|' + meeting.classId + '|' + str(recording_start) + '|' + meeting.teacherName + '|' + meeting.className + '###\n###YJv1###\n' + video['title']
     request = youtube.videos().update(
         part='snippet',
         body={
-            'id': video[0],
+            'id': video['id'],
             'snippet': {
                 'description': new_desc,
                 'title': new_title,
@@ -100,35 +108,81 @@ def process_video(video, meeting, youtube):
         }
     )
     request.execute()
-    meetingdb.log("Updated video %s %s" % (video[0], new_title))
-    print(video[2])
-    request = youtube.playlistItems().delete(id=video[2])
-    request.execute()
-    meetingdb.log("Removed video %s from unprocessed playlist" % video[0])
-
-    # TODO: use an in-memory PlaylistManager in stead of reading file everytime.
-    pl_open = open(playlist_json, mode='r')
-    read = json.load(pl_open)
-    pl_open.close()
-    pl_id = read[meeting.classId]
-    # TODO: if pl_id is None, create a new playlist
+    log("Updated video %s %s" % (video['id'], new_title))
 
     request = youtube.playlistItems().insert(
         part='snippet',
         body={
             'snippet': {
-                'playlistId': pl_id,
-                # 'position': 0,
+                'playlistId': meeting.playlist,
                 'resourceId': {
                 'kind': 'youtube#video',
-                'videoId': video[0]
+                'videoId': video['id']
                 }
             }
         }
     )
     request.execute()
-    meetingdb.log("Add video %s %s into playlist %s" % (video[0], new_title, pl_id))
+    log("Add video %s %s into playlist %s" % (video['id'], new_title, meeting.playlist))
 
+    request = youtube.playlistItems().delete(id=video['itemId'])
+    request.execute()
+    log("Removed video %s from unprocessed playlist" % video['id'])
+
+def process_unmatched_video(youtube, video):
+    new_desc = '''###YJv1:video does not match any meeting###\n%s''' % (video['desc'])
+    request = youtube.videos().update(
+        part='snippet',
+        body={
+            'id': video['id'],
+            'snippet': {
+                'title': video['title'],
+                'description': new_desc,
+                'categoryId': 22
+            }
+        }
+    )
+    request.execute()
+    log('Updated unmatched video %s description' % video["id"])
+    request = youtube.playlistItems().insert(
+        part="snippet",
+        body={
+            "snippet": {
+                "playlistId": error_processing_playlist,
+                "resourceId": {
+                    "kind": "youtube#video",
+                    "videoId": video['id']
+                }
+            }
+        }
+    )
+    request.execute()
+    log('Added video %s to error processing playlist' % video['id'])
+    request = youtube.playlistItems().delete(id=video['itemId'])
+    request.execute()
+    log('Removed video %s from unprocessed playlist' % video['id'])
+
+def make_playlist(youtube, tlclass):
+    title = tlclass.className + ' | ' + tlclass.teacherName
+    desc = """###YJv1:%s:%s###""" % (tlclass.classId, tlclass.className)
+
+    request = youtube.playlists().insert(
+        part='snippet,status',
+        body={
+            'snippet': {
+                'title': title,
+                'description': desc,
+                'tags': [],
+                'defaultLanguage': 'en'
+            },
+            'status': {
+                'privacyStatus': 'unlisted'
+            }
+        }
+    )
+    response = request.execute()
+    log("Created a new playlist %s : %s" % (response['id'], title))
+    return response['id']
 
 
 def run_main(parser, options, args, output=sys.stdout):
@@ -137,6 +191,11 @@ def run_main(parser, options, args, output=sys.stdout):
     if not youtube:
         raise AuthenticationError("Cannot get youtube resource")
 
+    playlist_json_file = options.playlist_json or 'data/playlist.json'
+    playlistDB = classes.PlaylistDB(playlist_json_file)
+    meeting_json_file = options.meeting_json or 'data/meetings.json'
+    meetingDB = classes.MeetingDB(meeting_json_file, playlistDB)
+
     # call thinkland module to load the meetingDB object
     # meetingDB = None  # placeholder
     # playlistManager = None
@@ -144,24 +203,41 @@ def run_main(parser, options, args, output=sys.stdout):
     unprocessed_videos = get_some_unprocessed_videos(youtube)
     youtube_points = 0
     v_processed = 0
+    error_processed = 0
+
     for video in unprocessed_videos:
-        meeting = meetingdb.match(video[1], meetingdb_json, 20, False)
+        meeting = meetingDB.match(video['title'], 20)
         if meeting is None:
-            meetingdb.log("Meeting not found: " + video[1])            
-            # TODO: update the video descritiop, and move to a ErrorProcessing playlist
+            log("Meeting not found: " + video['title'])            
+            # TODO: update the video descrition, and move to a ErrorProcessing playlist
+            process_unmatched_video(youtube, video)
+            youtube_points += 160
+            error_processed += 1
+            if youtube_points > DAILY_THRESHOLD:
+                print("Reached daily youtube points threshold: %d" % youtube_points)
+                break
             continue
+
+        if meeting.playlist is None:
+            meeting.playlist = playlistDB.getPlaylist(meeting.classId)
+        if meeting.playlist is None:
+            meeting.playlist = make_playlist(youtube, meeting)
+            youtube_points += 50
+            playlistDB.addPlaylist(meeting, meeting.playlist)
+            playlistDB.writeBack()
         
         process_video(video, meeting, youtube)
+        meeting.youtubeURL = f'https://youtube.com/watch?v={video["id"]}'
         v_processed += 1
     
         youtube_points += 160
         if youtube_points > DAILY_THRESHOLD:
+            print("Reached daily youtube points threshold: %d" % youtube_points)
             break
 
-    # meetingdb.write_back()
-
-    print('Videos processed: ')
-    print(v_processed)        
+    meetingDB.writeBack()
+    print('Videos processed: ' + str(v_processed))
+    print('Error processed: ' + str(error_processed))
 
 
 def main(arguments):
@@ -175,6 +251,12 @@ def main(arguments):
                       type="string", help='Credentials JSON file')
     parser.add_option('', '--auth-browser', dest='auth_browser', action='store_true',
                       help='Open a GUI browser to authenticate if required')
+
+    # Business specific flags
+    parser.add_option('', '--meeting_json', dest='meeting_json',
+                      type="string", help='path to the json file of meetingDB')
+    parser.add_option('', '--playlist_json', dest = 'playlist_json',
+                      type='string', help='path to the json file of playlistDB')
 
     options, args = parser.parse_args(arguments)
 
